18b317818a9ed1c02e543d16260de3fecf52eb39
Wojciech <kowalczyk.wojtek168@gmail.com>
2023-04-03 23:12:26 +0200

    db attached

--- motoservice.sql
+++ motoservice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 28 Lut 2023, 21:07
+-- Czas generowania: 03 Kwi 2023, 23:11
 -- Wersja serwera: 10.4.10-MariaDB
 -- Wersja PHP: 7.3.12
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `img_src` text DEFAULT NULL,
+  `title` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `services`
+--
+
+INSERT INTO `services` (`id`, `img_src`, `title`, `description`, `price`) VALUES
+(1, 'https://images.unsplash.com/photo-1636761358954-cebc0e5dfebb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW90byUyMHJlcGFpcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', 'Podstawowy przegląd', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur dapibus tortor sit amet tristique. Praesent tempor ex at vulputate.', 100);
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `users`
 --
 
@@ -39,11 +60,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`) VALUES
-(1, 'adam123', '$2y$10$fDAFNlYo8uuV3lF1F/4moejKe/ewxXjvx1XvYBEaeTifATkdoQNBi');
+(1, 'adam123', '$2y$10$fDAFNlYo8uuV3lF1F/4moejKe/ewxXjvx1XvYBEaeTifATkdoQNBi'),
+(2, 'adam', '$2y$10$RV44a50IfFSOwwf3Y9aQs.1VwBMpTIjkNqybpr2wRHuiM9Ofz9V/m'),
+(3, 'adas123', '$2y$10$HGGP/D7e8njqEPP8dYkN4.nsRdrmx9GWj36it6dASC6hy1VPLFTo6');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -56,10 +85,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
