c41952d593548aa3721cae7f414d1fc160a8f566
Wojciech <kowalczyk.wojtek168@gmail.com>
2023-06-04 18:31:39 +0200

    db added

--- motoservice.sql
+++ motoservice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 03 Kwi 2023, 23:11
+-- Czas generowania: 04 Cze 2023, 18:31
 -- Wersja serwera: 10.4.10-MariaDB
 -- Wersja PHP: 7.3.12
 
@@ -21,6 +21,25 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `motoservice`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `login` text DEFAULT NULL,
+  `pswd` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `pswd`) VALUES
+(1, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -62,11 +81,42 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `login`, `password`) VALUES
 (1, 'adam123', '$2y$10$fDAFNlYo8uuV3lF1F/4moejKe/ewxXjvx1XvYBEaeTifATkdoQNBi'),
 (2, 'adam', '$2y$10$RV44a50IfFSOwwf3Y9aQs.1VwBMpTIjkNqybpr2wRHuiM9Ofz9V/m'),
-(3, 'adas123', '$2y$10$HGGP/D7e8njqEPP8dYkN4.nsRdrmx9GWj36it6dASC6hy1VPLFTo6');
+(3, 'adas123', '$2y$10$HGGP/D7e8njqEPP8dYkN4.nsRdrmx9GWj36it6dASC6hy1VPLFTo6'),
+(4, 'dzonyrozpierdolony', '$2y$10$qUL/Mej1vNvF1MvR5hg9pOB.K70o2UhjIaED3aOxguupF5Uo2lvGC'),
+(5, 'alebolecalemotorzysta', '$2y$10$vvisyvuih0joAFuzKHQEjedc1xtMga5e7B6QCTfcEoQNalaqqVdh.');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL,
+  `model` text DEFAULT NULL,
+  `prod_year` int(11) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `img_src` text DEFAULT NULL,
+  `owner` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `vehicles`
+--
+
+INSERT INTO `vehicles` (`id`, `model`, `prod_year`, `status`, `img_src`, `owner`) VALUES
+(1, 'Honda pińcetka', 2018, 'nieuszkodzony', 'https://images.unsplash.com/photo-1542683088-abb3da334598?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8SE9OREElMjBNT1RPUkJJS0V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60', 'adam123'),
+(2, 'kawasaka bolca', 2010, 'nieuszkodzony', 'https://images.unsplash.com/photo-1607187488541-0ee24bd99a73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2F3YXNha2l8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60', 'adam123');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeksy dla tabeli `services`
@@ -81,8 +131,20 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT dla tabel zrzutów
 --
+
+--
+-- AUTO_INCREMENT dla tabeli `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `services`
@@ -94,7 +156,13 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT dla tabeli `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
